0827dc00059de30053b7afb1527d554d692909f0
fooisntcat <fooweihongfwh@gmail.com>
2025-08-16 18:08:41 +0800

    Used old input system
    Also changed the wand to a cylinder

--- Assets/Project/Scripts/DiceThrowerScript.cs
+++ Assets/Project/Scripts/DiceThrowerScript.cs
@@ -1,7 +1,11 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
+using TiltFive;
 
 public class DiceThrowerScript : MonoBehaviour
 {
@@ -21,31 +25,35 @@ public class DiceThrowerScript : MonoBehaviour
         {
             transform.position = wandLocation.position;
         }
-        if (Input.GetKeyDown(KeyCode.Space) && !_hasStoppedRolling)
+        // if (Input.GetKeyDown(KeyCode.Space) && !_hasStoppedRolling)
+        if (TiltFive.Input.GetButtonDown(TiltFive.Input.WandButton.A) && !_hasStoppedRolling)
+        {
+            foreach (var die in _spawnedDice)
             {
-                foreach (var die in _spawnedDice)
-                {
-                    Destroy(die);
-                }
-
-                //await Task.Delay(1000);
-                RollDice();
+                Destroy(die);
             }
+
+            //await Task.Delay(1000);
+            RollDice();
+        }
     }
 
-    private async void RollDice()
+    public async void RollDice()
     {
         if (dice == null) return;
 
-        for (int i = 0; i < amountOfDice; i++)
+        if (_isRolling == false)
         {
-            var newDice = Instantiate(dice, transform.position, transform.rotation);
-            _spawnedDice.Add(newDice.gameObject);
-            newDice.RollDice(throwForce, rollForce, i);
-            await Task.Yield();
+            for (int i = 0; i < amountOfDice; i++)
+            {
+                var newDice = Instantiate(dice, transform.position, transform.rotation);
+                _spawnedDice.Add(newDice.gameObject);
+                newDice.RollDice(throwForce, rollForce, i);
+                await Task.Yield();
+            }
         }
     }
-    
+
     private void OnEnable()
     {
         DiceRollScript.OnDiceResult += OnDiceFinished;
@@ -66,5 +74,11 @@ public class DiceThrowerScript : MonoBehaviour
             Debug.Log("All dice finished rolling!");
         }
     }
+
+    // Testing XR Wand button:
+//     public void buttonA(InputAction.CallbackContext context)
+//     {
+//         Debug.Log("Button A" + context.phase);
+//     }
 
 }
