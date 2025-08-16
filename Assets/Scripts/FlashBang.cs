3ada4288d7378dfc2bb15aa0609afbc7905ac830
Spankton <sainatthrau.g@gmail.com>
2025-08-16 14:16:55 +0800

    added flashbang
    flashbang
    table deco

---
+++ Assets/Scripts/FlashBang.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashBang : MonoBehaviour
+{
+    public float throwForce = 10f;
+    public Image whiteScreen;
+    public AudioSource bangSound;
+    public AudioSource whiteNoise;
+    public float flashDuration = 2f;
+
+    private Rigidbody rb;
+    private Vector3 dragStartPos;
+    private bool isDragging = false;
+    private bool hasFlashed = false;
+
+    void Start()
+    {
+        Debug.Log("WhiteScreen reference: " + (whiteScreen != null));
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true; // Don't move until thrown
+    }
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+        dragStartPos = Input.mousePosition;
+    }
+
+    void OnMouseUp()
+    {
+        if (isDragging)
+        {
+            Vector3 dragEndPos = Input.mousePosition;
+            Vector3 dragVector = dragStartPos - dragEndPos;
+            rb.isKinematic = false;
+            rb.AddForce(-dragVector.normalized * throwForce, ForceMode.Impulse);
+            isDragging = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!hasFlashed && collision.gameObject.CompareTag("Table"))
+        {
+            StartCoroutine(FlashEffect());
+            Debug.Log("Flashbang triggered!");
+            //FlashEffect();
+            hasFlashed = true;
+        }
+    }
+
+    private System.Collections.IEnumerator FlashEffect()
+    {
+        // Play sounds
+        bangSound.Play();
+        whiteNoise.Play();
+
+        // Fade in white screen instantly
+        whiteScreen.color = new Color(1, 1, 1, 1);
+
+        // Wait flash duration
+        yield return new WaitForSeconds(flashDuration);
+
+        // Fade out
+        float t = 0;
+        while (t < 1f)
+        {
+            t += Time.deltaTime / 1f;
+            whiteScreen.color = new Color(1, 1, 1, Mathf.Lerp(1, 0, t));
+            yield return null;
+        }
+    }
+}
